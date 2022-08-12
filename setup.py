ae0405e02e79961b91256dd0a80ecb8b49e3662b
yamaceay <yamacerenay2001@gmail.com>
2022-08-12 12:22:08 +0200

    v1.0.7: Deployment

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyrcache",
+    version="1.0.7",
+    author="Yamac Eren Ay",
+    author_email="yamacerenay2001@gmail.com",
+    description="Python API for accessing RedisCache",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yamaceay/pyrcache",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=[
+          'requests==2.28.1'
+      ],
+    python_requires='>=3.8',
+)
